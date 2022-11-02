3b0bd351cdbb16c2696ee7179caa4dacbc6c83e5
ramil <ramil1-9-9-9@yandex.ru>
2022-11-02 22:17:07 +0300

    feat(relayer): add getters of keystore and connection

--- relayer/relayer.js
+++ relayer/relayer.js
@@ -1,6 +1,8 @@
 const nearAPI = require('near-api-js');
 const http = require('node:http');
 const qs = require('querystring');
+const fs = require("fs");
+const homedir = require("os").homedir();
 
 
 const serverConfig = {
@@ -9,13 +11,35 @@ const serverConfig = {
 };
 
 
-const config = {
-    contrantId: process.argv[2], // second commad line argument is contractId
+const nearConfig = {
+    ACCOUNT_ID: process.argv[2], // second command line argument is accountId
+    KEY_PATH: process.argv[3],
 };
 
 
-async function createAccount() {
-}
+const getKeyStore = function () {
+    const credentials = JSON.parse(fs.readFileSync(homedir + nearConfig.KEY_PATH));
+    const myKeyStore = new keyStores.InMemoryKeyStore();
+    myKeyStore.setKey(nearConnectionConfig.networkId, nearConfig.ACCOUNT_ID, KeyPair.fromString(credentials.private_key));
+    return myKeyStore;
+};
+
+
+const getNearConnection = async function () {
+    const nearConnectionConfig = {
+        networkId: "testnet",
+        keyStore: getKeyStore(), // first create a key store 
+        nodeUrl: "https://rpc.testnet.near.org",
+        walletUrl: "https://wallet.testnet.near.org",
+        helperUrl: "https://helper.testnet.near.org",
+        explorerUrl: "https://explorer.testnet.near.org",
+    };
+
+    const nearConnection = await nearAPI.connect(nearConnectionConfig);
+
+    return nearConnection;
+};
+
 
 const requestListener = function (req, res) {
     if (req.method == 'POST') {
@@ -41,10 +65,15 @@ const requestListener = function (req, res) {
 
 
 const main = async () => {
-    const server = http.createServer(requestListener);
-    server.listen(serverConfig.port, serverConfig.hostname, () => {
-        console.log(`Relayer is running on http://${serverConfig.hostname}:${serverConfig.port}`);
-    });
+
+    const nearConnection = await getNearConnection()
+
+
+
+    // const server = http.createServer(requestListener);
+    // server.listen(serverConfig.port, serverConfig.hostname, () => {
+    //     console.log(`Relayer is running on http://${serverConfig.hostname}:${serverConfig.port}`);
+    // });
 }   
 
 main()
